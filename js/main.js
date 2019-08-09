179071a06836ea564ebb48fd6394d9f878d7bfa4
ycy <568082704@qq.com>
2019-08-09 22:47:30 +0800

    完成会动的简历

--- js/main.js
+++ js/main.js
@@ -1,112 +1,87 @@
-/* 把代码写到#code和style标签里*/ 
-function writeCode(prefix,code,fn) {
-  let domCode=document.querySelector('#code')
-  let n=0
-  let id =setInterval(() => {
-    n+=1
-    domCode.innerHTML=
-      Prism.highlight(prefix+code.substring(0,n), Prism.languages.css, 'css')
-    styleTag.innerHTML=prefix+code.substring(0,n)
-    domCode.scrollTop=domCode.scrollHeight
-    if (n>=code.length) {
+/* 把代码写到#code和style标签里*/
+function writeCss(prefix, code, fn) {
+  let domCode = document.querySelector("#code")
+  let n = 0
+  let id = setInterval(() => {
+    n += 1
+    domCode.innerHTML = Prism.highlight(
+      prefix + code.substring(0, n),
+      Prism.languages.css,
+      'css'
+    )
+    styleTag.innerHTML = prefix + code.substring(0, n)
+    domCode.scrollTop = domCode.scrollHeight
+    if (n >= code.length) {
       window.clearInterval(id)
-      fn&&fn.call()
-    } 
-  }, 0);
+      fn && fn.call()
+    }
+  }, 20)
 }
 
-function writeMarkdown(markdown,fn) {
-  let domPaper=document.querySelector('#paper>.content')
-  let n=0
-  let id =setInterval(() => {
-    n+=1
-    domPaper.innerHTML=markdown.substring(0,n)
-    domPaper.scrollTop=domPaper.scrollHeight
-    if (n>=markdown.length) {
-    window.clearInterval(id)
-    fn&&fn.call()
-    } 
-  }, 0);
+function writeMarkdown(markdown, fn) {
+  let domPaper = document.querySelector("#paper>.content")
+  let n = 0
+  let id = setInterval(() => {
+    n += 1
+    domPaper.innerHTML = markdown.substring(0, n)
+    domPaper.scrollTop = domPaper.scrollHeight
+    if (n >= markdown.length) {
+      window.clearInterval(id)
+      fn && fn.call()
+    }
+  }, 20)
 }
 
-
-
-var result=`/*
+var result = `/*
  *面试官你好，我是余晨阳
  *只用文字介绍太单调
  *我就用代码来介绍吧
  *首先准备一些样式*/
 
- *{
+*{
    transition:all 1s;
  }
 html{
-  background: rgb(222,222,222);
-  font-size:16px;
+  background: #eee;
 }
 #code{
-  border:1px solid red;
+  border:1px solid #aaa;
   padding:16px;
 }
 
 /* 我需要一些代码高亮 */
  
-.token.selector{
-  color:#690;
-}
-.token.property{
- color:#905;
-}
-.token.function{
- color:#dd4a68;
+.token.selector{color:#690;}
+.token.property{color:#905;}
+.token.function{color:#dd4a68;}
+
+/* 加一个呼吸效果*/
+#code{
+  animation: breath 0.5s infinite alternate-reverse;
 }
 
-/* 加点3D 效果*/
-#code{
-  transform:rotate(360deg);
-}
-/* 不玩了，我来介绍一下我自己吧 */
+/* 我来介绍一下我自己吧 */
+
 /* 我需要一张白纸 */
 
-#code{
-  position:fixed;
-  left:0;
-  width:50%;
-  height:100%;
+#code-wrapper{
+  width: 50%; left: 0; position: fixed; 
+  height: 100%;
 }
-#paper{
-  position:fixed;
-  right:0;
-  width:50%;
-  height:100%;
-  background:black;
-  display:flex;
-  justify-content:center;
-  align-items:center;
-  padding:16px;
-}
-#paper> .content{
-
-  background:white;
-  height:100%;
-  width:100%;
+#paper > .content {
+  display: block;
 }
 
+/* 于是我就可以在白纸上写字了，请看右边 */
 `
-var result2=`
+var result2 = `
 /*
-*接下来把markdown变成HTML-marked.js
-*/
-/*
-*接下给HTML加样式
-*/
-/*
-*这就是我的会动的简历了
-*谢谢观看
+*接下来用一个优秀的库 marked.js
+*把markdown变成HTML
 */
 `
 
-var md=`
+var md = `
 # 自我介绍
 
 我叫余晨阳
@@ -115,79 +90,62 @@ var md=`
 自学前端半年
 希望应聘前端开发岗位
 
-#技能介绍
+# 技能介绍
 
 熟悉JavaScript CSS
 
-#项目介绍
+# 项目介绍
 
-1.无缝轮播图
-2.web简历
-3.canvas画板
+1. 无缝轮播图
+2. web简历
+3. canvas画板
 
-#联系方式
+# 联系方式
 
-QQ 568082704
-Email 568082704@qq.com
-手机 13215805887
+- 微信 ycy568082704
+- Email 568082704@qq.com
+- 手机 13215805887
+
 
 `
-
-let result3=`
+let result3 = `
 /*
-* 这就是我的会动的简历
-* 谢谢观看
+*这就是我的会动的简历了
+*谢谢观看
 */
 `
 
 
-writeCode('',result,()=>{
-  creatPaper(()=>{
-    writeCode(result,result2,()=>{
-      writeMarkdown(md,()=>{})
+writeCss("", result, () => {
+  createPaper(() => {
+    writeMarkdown(md, () => {
+      writeCss(result, result2, () => {
+        convertMarkdownToHtml(() => {
+          writeCss(result + result2, result3, () => {})
+        })
+      })
     })
   })
 })
 
-
-function creatPaper(fn) {
-  var paper =document.createElement('div')
-  paper.id='paper'
-  var content =document.createElement('pre')
-  content.className='content'
+function createPaper(fn) {
+  var paper = document.createElement("div")
+  paper.id = "paper"
+  var content = document.createElement("pre")
+  content.className = "content"
   paper.appendChild(content)
   document.body.appendChild(paper)
-  fn&&fn()
+  fn && fn.call()
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function convertMarkdownToHtml(fn) {
+  var div = document.createElement("div")
+  div.className = "html markdown-body"
+  div.innerHTML = marked(md)
+  let markdownContainer = document.querySelector("#paper>.content")
+  markdownContainer.replaceWith(div)
+  fn && fn.call()
+}
 
 
 
